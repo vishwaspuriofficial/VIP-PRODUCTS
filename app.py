e923d58911692f9703c5cf19141ec2abc4d756c7
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-30 11:28:37 -0400

    updated

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ import home
 # Create an instance of the app
 app = MultiPage()
 
-# Title of the main page
-st.title("Multi-page Application")
-
 # Add all your applications (pages) here
 app.add_page("Home", home.app)
 app.add_page("Summarizer", vip_summarizer.app)
