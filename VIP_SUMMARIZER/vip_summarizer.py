9bca77a7fcf2c51fe997ea99f9a34ddfd85eaa80
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-11-01 07:02:47 -0400

    minor errors fixed

--- VIP_SUMMARIZER/vip_summarizer.py
+++ VIP_SUMMARIZER/vip_summarizer.py
@@ -3,7 +3,6 @@ def app():
     from transformers import pipeline
     import time
     #import clipboard
-    import pyperclip
 
     def summary(text):
         summarizer = pipeline("summarization")
@@ -41,7 +40,7 @@ def app():
                 if result == "":
                     raise TypeError
                 state.b = output.text_area("Summarized Text: ", value=result, height=200, key="output_final")
-                pyperclip.copy(state.b)
+                #clipboard.copy(state.b)
                 #st.success("Output copied to clipboard!")
             except:
                 st.error("Error! Text could not be summarized.")
