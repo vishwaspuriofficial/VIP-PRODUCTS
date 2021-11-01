0d8b09a01d23afccd3193a9ee9aac5f005d78496
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-11-01 05:41:36 -0400

    gensim summarization faster

--- VIP_SUMMARIZER/vip_summarizer.py
+++ VIP_SUMMARIZER/vip_summarizer.py
@@ -1,53 +1,97 @@
+# def app():
+#     import streamlit as st
+#     from transformers import pipeline
+#     import time
+#     import clipboard
+#
+#     def summary(text):
+#         summarizer = pipeline("summarization")
+#
+#         summarized = summarizer(text, min_length=25, max_length=300)
+#
+#         for i in summarized:
+#             for j in i:
+#                 result = i[j]
+#                 return result
+#
+#     col1, col2 = st.columns(2)
+#
+#     text = col1.text_area("Enter Text: ",height=200, key="input_placeholder")
+#
+#     summarize = col1.button("Summarize")
+#
+#     state = st.session_state
+#
+#     c2= col2.empty()
+#     c1 = col1.empty()
+#
+#     output = c2.empty()
+#
+#     b = output.text_area("Summarized Text: ", height=200, key="output_placeholder")
+#     if summarize:
+#         if text == "":
+#             st.error("Error! No text entered.")
+#         elif len(text) <=50:
+#             st.error("Error! Insufficient text for summary.")
+#         else:
+#             try:
+#                 with st.spinner('Summarizing...'):
+#                     result = summary(text)
+#                 if result == "":
+#                     raise TypeError
+#                 state.b = output.text_area("Summarized Text: ", value=result, height=200, key="output_final")
+#                 clipboard.copy(state.b)
+#                 st.success("Output copied to clipboard!")
+#             except:
+#                 st.error("Error! Text could not be summarized.")
+#                 st.exception(RuntimeError)
+#
+#     time.sleep(2)
+#     c1.write("")
+#
+# if __name__ == "__main__":
+#     app()
+
 def app():
-    import streamlit as st
-    from transformers import pipeline
-    import time
-    import clipboard
+	import streamlit as st
+	from gensim.summarization import summarize
+	import time
+	import clipboard
 
-    def summary(text):
-        summarizer = pipeline("summarization")
+	col1, col2 = st.columns(2)
 
-        summarized = summarizer(text, min_length=25, max_length=300)
+	text = col1.text_area("Enter Text: ",height=200, key="input_placeholder")
 
-        for i in summarized:
-            for j in i:
-                result = i[j]
-                return result
+	summarizeBtn = col1.button("Summarize")
 
-    col1, col2 = st.columns(2)
+	state = st.session_state
 
-    text = col1.text_area("Enter Text: ",height=200, key="input_placeholder")
+	c2= col2.empty()
+	c1 = col1.empty()
 
-    summarize = col1.button("Summarize")
+	output = c2.empty()
 
-    state = st.session_state
+	b = output.text_area("Summarized Text: ", height=200, key="output_placeholder")
+	if summarizeBtn:
+		if text == "":
+			st.error("Error! No text entered.")
+		elif len(text) <=50:
+			st.error("Error! Insufficient text for summary.")
+		else:
+			try:
+				with st.spinner('Summarizing...'):
+					result = summarize(text)
+					st.write(result)
+				if result == "":
+					raise TypeError
+				state.b = output.text_area("Summarized Text: ", value=result, height=200, key="output_final")
+				clipboard.copy(state.b)
+				st.success("Output copied to clipboard!")
+			except:
+				st.error("Error! Text could not be summarized.")
 
-    c2= col2.empty()
-    c1 = col1.empty()
-
-    output = c2.empty()
-
-    b = output.text_area("Summarized Text: ", height=200, key="output_placeholder")
-    if summarize:
-        if text == "":
-            st.error("Error! No text entered.")
-        elif len(text) <=50:
-            st.error("Error! Insufficient text for summary.")
-        else:
-            try:
-                with st.spinner('Summarizing...'):
-                    result = summary(text)
-                if result == "":
-                    raise TypeError
-                state.b = output.text_area("Summarized Text: ", value=result, height=200, key="output_final")
-                clipboard.copy(state.b)
-                st.success("Output copied to clipboard!")
-            except:
-                st.error("Error! Text could not be summarized.")
-                st.exception(RuntimeError)
-
-    time.sleep(2)
-    c1.write("")
+	time.sleep(2)
+	c1.write("")
 
 if __name__ == "__main__":
-    app()
+	app()
