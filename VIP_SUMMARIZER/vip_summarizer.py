87bdd579befe9241137cda205038162272241bfb
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-11-01 04:58:03 -0400

    catch errors

--- VIP_SUMMARIZER/vip_summarizer.py
+++ VIP_SUMMARIZER/vip_summarizer.py
@@ -44,8 +44,7 @@ def app():
                 st.success("Output copied to clipboard!")
             except:
                 st.error("Error! Text could not be summarized.")
-                e = RuntimeError('This is an exception of type RuntimeError')
-                st.exception(e)
+                st.exception(RuntimeError)
 
     time.sleep(2)
     c1.write("")
