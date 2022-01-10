adfeb653c666b4699fba3e3ce2381efa9e6852c7
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-01-10 10:18:42 -0500

    updated

--- VIP_QR_GENERATOR/vip_qr_generator.py
+++ VIP_QR_GENERATOR/vip_qr_generator.py
@@ -1,15 +1,18 @@
 def app():
     import streamlit as st
-    import qrcode
+    # import qrcode
     from PIL import Image
     import numpy as np
+    from MyQR import myqr as mq
+
 
     text = st.text_input('Make a QR code of:')
     a = st.button("Create QR Code")
     if a:
         try:
-            img = qrcode.make(text)
-            img.save("qr.png")
+            mq.run(text, save_name='qr.png')
+            # img = qrcode.make(text)
+            # img.save("qr.png")
             st.subheader("Your QR Code is ready!")
             st.image("qr.png")
 
