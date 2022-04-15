a1ca10dee2a3aec5739ba18aa0de5ad6e42d2147
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-04-15 17:52:33 -0400

    updated

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "[theme]
-primaryColor=’#020202’
-backgroundColor=’#c4c3c3’
-secondaryBackgroundColor=’#ebd316’
+primaryColor = ‘#020202’
+backgroundColor = ‘#c4c3c3’
+secondaryBackgroundColor = ‘#ebd316’
 font = ‘sans serif’
 [server]
 headless = true
